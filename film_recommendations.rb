35acec0f840e4ae16f7dcd576b7b53a6417d5e4d
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 14:24:39 -0400

    Created variables for movie titles and questions to ask user for each movie category

---
+++ film_recommendations.rb
@@ -0,0 +1,32 @@
+#Think of a documentary, a drama, a comedy, and a dramedy. Store the titles of these films in variables.
+
+documentary = "Brain Games"
+drama = "Shameless"
+comedy = "The Other Guys"
+dramedy = "A Dramedy Title"
+
+#Ask the user if they enjoy 1. documentaries 2. dramas 3. comedies.
+
+puts "Hey there, let me ask you a few questions:"
+print "Do you enjoy Documentaries? (Yes or No) "
+documentary_answer = gets.chomp.downcase
+print "What about Dramas? (Yes or No) "
+drama_answer = gets.chomp.downcase
+print "How do you feel about Comedies? (Yes or No) "
+comedy_answer = gets.chomp.downcase
+print "And lastly, is Dramedies you thing? (Yes or No) "
+dramedies_answer = gets.chomp.downcase
+
+
+#If they answer yes to documentaries, display a message recommending the documentary to them.
+
+
+
+#If they answer no to documentaries but yes to dramas and comedies, recommend the dramedy.
+
+
+#If they answer yes to only dramas, recommend the drama.
+
+#If they say yes to only comedies, recommend the comedy.
+
+#If they answer no to all three, recommend a good book instead.
