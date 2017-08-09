394b32c1626a2bc14a6171d0a38052b6d26004f0
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 19:08:50 -0400

    Exercise2 - created variables for recommendation

---
+++ film_recommendations2.rb
@@ -0,0 +1,5 @@
+#create the same variables containing your potential film recommendations
+documentary = "Brain Games"
+drama = "Shameless"
+comedy = "The Other Guys"
+dramedy = "This Awesome Dramedy Movie!!"
